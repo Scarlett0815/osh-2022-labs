ddc15e387e885940824653dc747eaa97e4c5b1c5
Scarlett Anderson <rjx08150100@mail.ustc.edu.cn>
2022-05-24 17:46:57 +0800

    the detail mutex 2.c

--- lab3/2.c
+++ lab3/2.c
@@ -6,102 +6,113 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
+struct Str_news{
+    char str_small[2000];
+    struct Str_news *next;
+};
+
 struct Pipe {
     int fd;
+    struct Str_news *str_wait;
     struct Pipe *next;
 };
 
 int thread_index = 0;
 struct Pipe *head;
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+
+
+void *handle_send(void *data){
+    struct Pipe *pipe1 = (struct Pipe *)data;
+    int send_flag = 0;
+    int index = 0;
+    while (1){
+        index = 0;
+        if (pipe1 -> str_wait != NULL){
+            char buffer[10240];
+            struct Str_news *str1 = pipe1 -> str_wait;
+            strcpy(buffer,pipe1->str_wait->str_small);   
+            int len = strlen(pipe1->str_wait->str_small);
+            int tmp_stop = 0;
+            for (int i = 0;i < len;i ++){
+                if (buffer[i] == '\n'){
+                    if (send_flag == 0){
+                        char str[10240] = "Message:";
+                        for (int j = tmp_stop;j <= i;j ++){
+                            str[8 + j - tmp_stop] = buffer[j];
+                        } 
+                        send(pipe1 -> fd, str,i - tmp_stop + 9, 0);
+                    }
+                    else {
+                        char str[10240];
+                        for (int j = tmp_stop;j <= i;j ++){
+                            str[j - tmp_stop] = buffer[j];
+                        }
+                        send(pipe1 -> fd, str,i - tmp_stop + 1, 0);
+                    }
+                    tmp_stop = i + 1;  
+                    send_flag = 0;   
+                    //close(file);     
+                }
+            }
+            if (buffer[len - 1] == '\n') {index = 0;}
+            else {
+                for (int i = tmp_stop;i < len;i ++){
+                    buffer[i - tmp_stop] = buffer[i];
+                }
+                index = len - tmp_stop;
+                if (send_flag){
+                    send(pipe1 -> fd, buffer,index, 0);
+                }
+                else {
+                    char str[10240] = "Message:";
+                    for (int j = 0;j < index;j ++){
+                        str[8 + j] = buffer[j];
+                    }
+                    send(pipe1->fd, str,index + 8, 0);
+                    send_flag = 1;
+                }
+                index = 0;
+            }
+            pipe1 -> str_wait = str1 -> next;
+        }
+    }
+}
 
 void *handle_chat(void *data) {
     struct Pipe *pipe1 = (struct Pipe *)data;
     char buffer[2000];
-    char tmp[1000];
     ssize_t len;
     int index = 0;
     int send_flag = 0;
     int count = 0;
     //int jike = 0;
     //FILE * file = fopen("1_out.txt","w");
-    while ((len = recv(pipe1->fd, buffer + index, 1000, 0)) > 0){
-// send(pipe1->fd_recv, buffer, len + 8, 0);
-        //printf("%d\n",count++);
-        //fputs(buffer,file);
-        //if (jike) printf("kjsdlk\n");
-        int tmp_stop = 0;
-        for (int i = 0;i < len;i ++){
-            if (buffer[i] == '\n'){
-                if (send_flag == 0){
-                    char str[1024] = "Message:";
-                    for (int j = tmp_stop;j <= i;j ++){
-                        str[8 + j - tmp_stop] = buffer[j];
-                    }
-                    struct Pipe *q = pipe1 -> next;
-                    while (q != pipe1){
-                        pthread_mutex_lock(&mutex);
-                        send(q -> fd, str,i - tmp_stop + 9, 0);
-                        pthread_cond_signal(&cv);
-                        pthread_mutex_unlock(&mutex);
-                        q = q -> next;
-                    }
-                }
-                else {
-                    char str[1024];
-                    for (int j = tmp_stop;j <= i;j ++){
-                        str[j - tmp_stop] = buffer[j];
-                    }
-                    struct Pipe *q = pipe1 -> next;
-                    while (q != pipe1){
-                        pthread_mutex_lock(&mutex);
-                        send(q -> fd, str,i - tmp_stop + 1, 0);
-                        pthread_cond_signal(&cv);
-                        pthread_mutex_unlock(&mutex);
-                        q = q -> next;
-                    }
-                }
-                tmp_stop = i + 1;  
-                send_flag = 0;   
-                //close(file);     
+    while ((len = recv(pipe1->fd, buffer, 1000, 0)) > 0){
+        struct Str_news *str = malloc(sizeof(struct Str_news));
+        struct Pipe *q = pipe1 -> next;
+        strcat(str->str_small,buffer);
+        printf ("%s",buffer);
+        pthread_mutex_lock(&mutex);
+        while (q != pipe1){
+            struct Str_news *tail_search = q -> str_wait;
+            while (tail_search != NULL && tail_search -> next != NULL){ 
+                tail_search = tail_search -> next;
             }
+            if (tail_search == NULL){
+                q -> str_wait = str;
+                str -> next = NULL;
+            }
+            else{
+                tail_search -> next = str;
+                str -> next = NULL;
+            }
+            q = q->next;
         }
-        if (buffer[len - 1] == '\n') {index = 0;}
-        else {
-            for (int i = tmp_stop;i < len;i ++){
-                buffer[i - tmp_stop] = buffer[i];
-            }
-            index = len - tmp_stop;
-            if (index >= 500){
-                if (send_flag){
-                    struct Pipe *q = pipe1 -> next;
-                    while (q != pipe1){
-                        pthread_mutex_lock(&mutex);
-                        send(q -> fd, buffer,index, 0);
-                        pthread_cond_signal(&cv);
-                        pthread_mutex_unlock(&mutex);
-                        q = q -> next;
-                    }
-                }
-                else {
-                    char str[1024] = "Message:";
-                    for (int j = 0;j < index;j ++){
-                        str[8 + j] = buffer[j];
-                    }
-                    struct Pipe *q = pipe1 -> next;
-                    while (q != pipe1){
-                        pthread_mutex_lock(&mutex);
-                        send(q->fd, str,index + 8, 0);
-                        pthread_cond_signal(&cv);
-                        pthread_mutex_unlock(&mutex);
-                        q = q -> next;
-                    }
-                    send_flag = 1;
-                }
-                index = 0;
-            }
-        }
+        pthread_cond_signal(&cv);
+        pthread_mutex_unlock(&mutex);
+        memset(buffer,0,2000);
     }
     if (head == pipe1){
         head = pipe1 -> next;
@@ -112,7 +123,7 @@ void *handle_chat(void *data) {
     }
     q -> next = pipe1 -> next;
     thread_index --;
-    printf ("1");
+    //printf ("1");
     return NULL;
 }
 
@@ -140,6 +151,7 @@ int main(int argc, char **argv) {
 
 
     pthread_t thread[32];
+    pthread_t thread1[32];
     head = malloc(sizeof(struct Pipe));
     int fd_new;
     fd_new = accept(fd,NULL,NULL);
@@ -150,6 +162,7 @@ int main(int argc, char **argv) {
     head ->fd = fd_new;
     
     pthread_create(&thread[thread_index],NULL,handle_chat,(void *)head);
+    pthread_create(&thread1[thread_index],NULL,handle_send,(void *)head);
     
 
     thread_index ++;
@@ -163,8 +176,9 @@ int main(int argc, char **argv) {
             return 1;
         }
         pipe -> fd = fd_new; 
-        
+        pipe->str_wait = NULL;
         pthread_create(&thread[thread_index],NULL,handle_chat,(void *)pipe);
+        pthread_create(&thread1[thread_index],NULL,handle_send,(void *)pipe);
         pipe -> next = head -> next;
         head -> next = pipe;
         head = pipe;  
